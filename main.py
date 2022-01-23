e796c4af968f4539b2ff2693165a84983da446ef
Кавицян Лаврентий Михайлович <myworkpython@mail.ru>
2022-01-23 16:44:12 +0300

    .add readme

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas as pd
 from model import linearModel
 
 
+class Weights:
+    '''Затычка для демонстрации'''
+    pass
+
+
+def train(data: pd.DataFrame) -> Weights:
+    pass
+
+
 def score(data: pd.DataFrame) -> pd.DataFrame:
     dataValues = data.values
     return linearModel.predict(dataValues)
